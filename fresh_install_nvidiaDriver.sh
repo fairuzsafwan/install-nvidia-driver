7bf9d477855ce7cc77e4549a67988fd1d29bf60d
safwan <fsafwan88@gmail.com>
2022-02-08 11:11:42 +0900

    updated script

--- fresh_install_nvidiaDriver.sh
+++ fresh_install_nvidiaDriver.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+sudo apt-get purge nvidia-*
+sudo apt-get update
+sudo apt-get autoremove
+
 recommended=$(ubuntu-drivers devices | grep 'recommended')
 echo $recommended
 
@@ -15,7 +19,13 @@ version=${splittedWord[2]}
 #Validate if version is integer
 if ! [ "$version" -eq "$version" ] 2> /dev/null
 then
-    echo "Sorry integers only"
+    echo "Invalid version detected! : exiting, try manually inputting commands"
+    exit 1
 else
      echo "Version validated!"
 fi
+
+sudo apt install libnvidia-common-$version
+sudo apt install libnividia-gl-$version
+sudo apt install nvidia-driver-$version
+sudo reboot
